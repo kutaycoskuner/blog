84613d0e3ab5c85b81377e28fda4a7d45360200e
Kutay Coskuner <37559343+kutaycoskuner@users.noreply.github.com>
2026-07-21 14:07:31 +0200

    added essay navigator

---
+++ src/components/DevEssays.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let essays;
+</script>
+
+<div class="list">
+    {#each essays as essay}
+        <div class="entry">
+            <a
+                href={`${import.meta.env.BASE_URL}essays/${essay.id.replace(/\.(md|mdx)$/, "")}`}
+            >
+                {essay.data.title}
+            </a>
+        </div>
+    {/each}
+</div>
+
+<style scoped>
+    .list {
+        display: flex;
+        flex-direction: column;
+        font-family: monospace;
+    }
+
+    a {
+        color: var(--col);
+        text-decoration: none;
+    }
+
+    a:hover {
+        text-decoration: underline;
+        opacity: 0.8;
+    }
+
+    .entry {
+        margin-bottom: 0.4rem;
+    }
+</style>
